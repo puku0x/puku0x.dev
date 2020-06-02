8e28872faec4fa750dfda3a2cf552bba6d59b6b5
puku0x <puku0x@gmail.com>
2020-06-03 01:12:54 +0900

    feat(app): add route transition animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
+import {
+  trigger,
+  animate,
+  transition,
+  style,
+  query,
+} from '@angular/animations';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+export const fadeInAnimation = trigger('fadeInAnimation', [
+  transition('* => *', [
+    query(
+      ':enter',
+      [style({ opacity: 0 }), animate('250ms', style({ opacity: 1 }))],
+      { optional: true }
+    ),
+  ]),
+]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [fadeInAnimation],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  getRouterOutletState(outlet: RouterOutlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
+  }
+}
