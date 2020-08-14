9a3025112074e60c72249e201b344ca3550efcd4
puku0x <puku0x@gmail.com>
2020-08-14 16:10:27 +0900

    feat(*): add seoHrefOptimise plugin

--- scully.website.config.ts
+++ scully.website.config.ts
@@ -18,6 +18,6 @@ export const config: ScullyConfig = {
       },
     },
   },
-  defaultPostRenderers: [DisableAngular],
+  defaultPostRenderers: ['seoHrefOptimise', DisableAngular],
   // defaultPostRenderers: ['seoHrefOptimise', flashPrevention],
 };
