d9402fdfe3578f37731e7c89c3363d8b8167e3f8
puku0x <puku0x@gmail.com>
2020-10-03 17:54:13 +0900

    feat(plugins): add workbox plugin

---
+++ scully/plugins/workbox.ts
@@ -0,0 +1,61 @@
+import { registerPlugin, getPluginConfig } from '@scullyio/scully';
+
+export const workboxPlugin = 'workboxPlugin';
+
+export interface WorkboxPluginConfig {
+  swPath?: string;
+}
+
+const defaultConfig: WorkboxPluginConfig = {
+  swPath: '/service-worker.js',
+};
+
+const plugin = async (html: string): Promise<string> => {
+  const config: WorkboxPluginConfig = Object.assign(
+    {},
+    defaultConfig,
+    getPluginConfig(workboxPlugin)
+  );
+
+  const workboxScript = `
+<script defer scullyKeep>
+  (() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('${config.swPath}')
+          .then((registration) => {
+            registration.addEventListener('updatefound', () => {
+              const installingWorker = registration.installing;
+              if (!installingWorker) return;
+              installingWorker.addEventListener('statechange', () => {
+                switch (installingWorker.state) {
+                  case 'installed':
+                    if (navigator.serviceWorker.controller) {
+                      installingWorker.postMessage({
+                        type: 'SKIP_WAITING',
+                      });
+                    }
+                    break;
+                }
+              });
+            });
+          });
+      });
+      let refreshing = false;
+      navigator.serviceWorker.addEventListener('controllerchange', () => {
+        if (refreshing) return;
+        window.location.reload();
+        refreshing = true;
+      });
+    }
+  })();
+</script>
+`;
+
+  return html.replace(/<\/head/i, `${workboxScript}</head`);
+};
+
+const validator = async () => [];
+
+registerPlugin('render', workboxPlugin, plugin, validator);
