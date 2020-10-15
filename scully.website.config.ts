53e4ce18a2f3a8d492bdf007b5674d1d70051325
puku0x <puku0x@gmail.com>
2020-10-16 01:23:15 +0900

    feat(*): add clipboard plugin

--- scully.website.config.ts
+++ scully.website.config.ts
@@ -2,6 +2,10 @@ import { ScullyConfig, prod, setPluginConfig } from '@scullyio/scully';
 import { MarkedConfig } from '@scullyio/scully/lib/fileHanderPlugins/markdown';
 import { criticalCSS } from '@scullyio/scully-plugin-critical-css';
 import {
+  CopyToClipboard,
+  CopyToClipboardPluginConfig,
+} from '@scullyio/scully-plugin-copy-to-clipboard';
+import {
   removeScripts,
   RemoveScriptsConfig,
 } from '@scullyio/plugins-scully-plugin-remove-scripts';
@@ -11,14 +15,22 @@ import { WorkboxPluginConfig, workboxPlugin } from './scully/plugins/workbox';
 setPluginConfig<MarkedConfig>('md', {
   enableSyntaxHighlighting: true,
 });
+setPluginConfig<CopyToClipboardPluginConfig>(CopyToClipboard, {
+  customBtnClass: 'copy-to-clipboard',
+});
 setPluginConfig<RemoveScriptsConfig>(removeScripts, {
   keepTransferstate: false,
 });
 setPluginConfig<WorkboxPluginConfig>(workboxPlugin, {
-  swPath: '//service-worker.js',
+  swPath: '/service-worker.js',
 });
 
-const defaultPostRenderers = [removeScripts, 'seoHrefOptimise', criticalCSS];
+const defaultPostRenderers = [
+  removeScripts,
+  'seoHrefOptimise',
+  criticalCSS,
+  CopyToClipboard,
+];
 
 if (prod) {
   defaultPostRenderers.push(workboxPlugin);
@@ -33,6 +45,13 @@ export const config: ScullyConfig = {
       type: 'contentFolder',
       slug: {
         folder: './posts',
+      },
+    },
+    '/users/:userId': {
+      type: 'json',
+      userId: {
+        url: 'https://jsonplaceholder.typicode.com/users',
+        property: 'id',
       },
     },
   },
