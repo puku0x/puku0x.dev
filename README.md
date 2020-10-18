c59191e2a7cd1fedaab8a572a5848036ac7b7b45
puku0x <puku0x@gmail.com>
2020-10-18 15:59:58 +0900

    chore(*): update angular v11-next.6

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # puku0x.net
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.x.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.0-next.x.
 
 ## Install
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,16 +8,18 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('website app is running!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getTitleText()).toEqual('website app is running!');
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
